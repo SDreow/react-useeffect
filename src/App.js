2dc1bd662c3afbeca55c02ba6c68c0ccecac8d1f
Štěpán Velínský <stteranv@gmail.com>
2024-02-07 23:09:45 +0100

    Finaly step .then + API

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import { useState, useEffect } from "react"
 const url = "http://api.open-notify.org/iss-now.json"
 
 const App = () => {
-  const [loading, setLoading] = useState (false)
+  const [loading, setLoading] = useState (true)
+  const [latitude, setLatitude] =useState("")
+  const [longitude, setLongitude] =useState("")
   
   useEffect( () => {
       fetch(url) //then = počkej a ulož
         .then( (response) => response.json() )
         .then( (data) => data["iss_position"] )
         .then( (position) => {
-            console.log(position["latitude"])
-            console.log(position["longitude"])
+            setLatitude(position["latitude"])
+            setLongitude(position["longitude"])
         } )
+      setLoading(false)  
   }, [])
 
-  if (loading){
+  if(loading){
     return <>
     <h2>Načítání stránky...</h2>
   </>
-  }else {
-    return <>
-    <h1>Obsah stránky</h1>
-  </>
   }
+
+    return <>
+    <h1>Zeměpisná šířka</h1>
+    <p>{latitude}</p>
+    <h1>Zeměpisná délka</h1>
+    <p>{longitude}</p>
+  </>
+  
 
 }
 
