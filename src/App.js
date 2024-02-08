a4ce4c116019c6944c6f6a4bdcb3a2c5960aef3e
Štěpán Velínský <stteranv@gmail.com>
2024-02-08 10:05:11 +0100

    Ternar operator - example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-//Short circuit evaluation
+// Ternární operátor
+
 const App = () => {
-  const value = "david"
-  const value2 = 0
-
-  const result1 = value && value2
-  //první je true a druhé jakékoliv = se vrací druhá hodnota
-  //první je false a druhé jakékoliv = vrací se první hodnota
-  // console.log(result1)ˇ
-
-  const result2 = value || "tomas"
-  //první je true a druhé jakékoliv = vrací první
-  //první je false, tak vrací druhou hodnotu
-  console.log(result2)
-
+  const error = true
   return<>
-    <h1>{value2 || "Defalutní text"}</h1>
-    {value2 && <h1>Super tajný nadpis</h1>}
+    {error ? <p>Chyba</p> : <h2>Obsah stránky</h2> }
   </>
 }
 
